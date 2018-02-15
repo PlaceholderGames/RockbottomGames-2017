55475360208faf473693e12b6afafcbcf7ee44f5
Joshua Ravenscroft <Joshua.Ravenscroft@yahoo.co.uk>
2018-02-15 17:12:05 +0000

    Updated movement
    changed movement to work with rigidbody and added jump.

--- Assets/Scripts/Movement/Movement.cs
+++ Assets/Scripts/Movement/Movement.cs
@@ -2,41 +2,81 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
+
 public class Movement : MonoBehaviour {
 
     private float speed = 10.0f;
     private float walkSpeed = 10.0f;
     private float runSpeed = 25.0f;
     private float gravity = 10f;
-    private CharacterController charCont;
+    public float maxVelocityChange = 10.0f;
+    float jumpHeight = 2.0f;
+    public Rigidbody rb;
+    public Boundary boundary;
+
+    bool jump = true;
+    bool grounded = false;
 
     // Use this for initialization
     void Start () {
-        charCont = GetComponent<CharacterController>();
+        //charCont = GetComponent<CharacterController>();
 	}
 
-    // Update is called once per frame
-    void Update ()
+    void Awake()
+    {
+        rb.freezeRotation = true;
+        rb.useGravity = false;
+    }
+
+    // Update 
+    void FixedUpdate ()
     {   
-        // set the movement for the character.
-        float deltaX = Input.GetAxis("Horizontal") * speed;
-        float deltaZ = Input.GetAxis("Vertical") * speed;
+        if (grounded)
+        {
+            Vector3 velocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            velocity = transform.TransformDirection(velocity);
+            velocity *= speed;
 
-        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
-        movement = Vector3.ClampMagnitude(movement, speed); //Limits the max speed of the player
+            sprint();
 
-        movement.y -= gravity;
+            Vector3 vel = rb.velocity;
+            Vector3 velChange = (velocity - vel);
+            velChange.x = Mathf.Clamp(velChange.x, -maxVelocityChange, maxVelocityChange);
+            velChange.z = Mathf.Clamp(velChange.z, -maxVelocityChange, maxVelocityChange);
+            velChange.y = 0;
+            rb.AddForce(velChange, ForceMode.VelocityChange);
 
-        movement *= Time.deltaTime; //Ensures the speed the player moves doesn't change based on the frame rate
-        movement = transform.TransformDirection(movement);
-        charCont.Move(movement);
-        sprint(); // actives the sprint function. 
-	}
+            if (jump && Input.GetButton("Jump"))
+            {
+                rb.velocity = new Vector3(velocity.x, CalculateJumpVerticalSpeed(), velocity.z);
+            }
+        }
+
+        rb.AddForce(new Vector3(0, -gravity * rb.mass, 0));
+
+        grounded = false;
+    }
+
     void sprint()
     {
         if (Input.GetKey(KeyCode.LeftShift))
             speed = runSpeed; // sets the speed to a run.
         else
             speed = walkSpeed; // set it back to a walk.
+    }
+
+    void OnCollisionStay()
+    {
+        grounded = true;
+    }
+
+    float CalculateJumpVerticalSpeed()
+    {
+        return Mathf.Sqrt(2 * jumpHeight * gravity);
     }
 }
