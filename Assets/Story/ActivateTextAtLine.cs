d5103107d64b3a60849a92944adf8f1ab5f289fb
MunchMawr <16013808@students.southwales.ac.uk>
2018-04-19 23:51:09 +0100

    Script started for the Narr
    Scripts + Text File + Panel Icon

---
+++ Assets/Story/ActivateTextAtLine.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateTextAtLine : MonoBehaviour {
+    public TextAsset theText;
+
+    public int startLine;
+    public int endLine;
+
+    public TextBoxManager theTextBox;
+
+    public bool destroyWhenActivated;
+
+
+	// Use this for initialization
+	void Start () {
+        theTextBox = FindObjectOfType<TextBoxManager>();
+
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "Pla")
+        {
+            theTextBox.ReloadScript(theText);
+            theTextBox.currentLine = startLine;
+            theTextBox.endAtLine = endLine;
+        }
+
+        if(destroyWhenActivated)
+        {
+            Destroy(gameObject);
+        }
+
+     
+    }
+}
--- Assets/Story/TextBoxManager.cs
+++ Assets/Story/TextBoxManager.cs
@@ -14,22 +14,47 @@ public class TextBoxManager : MonoBehaviour {
 
     public int currentLine;
     public int endAtLine;
-    
+
+    public bool isActive;
+
+    // public GameObject Player;
+
+    Rigidbody rb;
+    public GameObject Player;
+
+    //Moving the doors
+    Transform tfDoors;
+    public GameObject Doors;
 
 
     // Use this for initialization
     void Start()
     {
+        rb = Player.GetComponent<Rigidbody>();     //Gets players rigidbody
+        tfDoors = Doors.GetComponent<Transform>();
 
+        /////////////////////////////////////////////
+        //First Load
+
+        MakeKinemtic();
+    
         if (textFile != null)
         {
             textLines = (textFile.text.Split('\n'));
         }
+        
 
-        if(endAtLine == 0)      //if set to 0 then read all not nothing.
+        if (endAtLine == 0)      //if set to 0 then read all not nothing.
         {
             endAtLine = textLines.Length - 1;
         }
+
+        if (isActive)
+        {
+            EnableTextBox();
+        }
+        else
+            DisableTextBox();
 
     }
 
@@ -38,14 +63,66 @@ public class TextBoxManager : MonoBehaviour {
     {
         theText.text = textLines[currentLine];
 
-        if(Input.GetKeyDown(KeyCode.Return))        //Press enter to navigate, 
+       
+        if(!isActive)        //if text is not active then dont run.
+        {
+            return;
+        }
+        
+
+        if (Input.GetKeyDown(KeyCode.Return))        //Press enter to navigate, 
         {                                           //throught the text.
-            currentLine += 1;       
+            currentLine += 1;
         }
 
-        if(currentLine > endAtLine)
+        if (currentLine > endAtLine)
         {
-            textBox.SetActive(false);   //Stops at the current set endline
+            DisableTextBox();   //Stops at the current set endline
+        }
+
+
+        if(currentLine == 17)       //Allows the player to move out of the tut.
+        {
+            Disableinemtic();
+        }
+
+        if(currentLine == 33)
+        {
+            Debug.Log("Line28");
+            tfDoors.position = new Vector3(0.0f, 8f, 0.0f);
+           
+        }
+
+    }
+
+    public void EnableTextBox()
+    {
+        textBox.SetActive(true);
+    }
+
+    public void DisableTextBox()
+    {
+        textBox.SetActive(false);
+    }
+
+    public void MakeKinemtic()
+    {
+        Debug.Log("Make");
+        rb.isKinematic = true;
+    }
+
+    public void Disableinemtic()
+    {
+        rb.isKinematic = false;
+    }
+
+    public void ReloadScript(TextAsset theText)
+    {
+        if(theText !=null)
+        {
+            textLines = new string[1];
+            textLines = (theText.text.Split('\n'));
+
         }
     }
 }
